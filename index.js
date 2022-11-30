a43f17b15aaf3688a30c271a96daaa3e5fdce8bb
Manthan Ankolekar <manthan.ank@gmail.com>
2022-11-30 15:47:49 +0530

    changes made

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
 // set up our express app
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(
-    cors({
-        origin: '*'
-    })
-)
+  cors({
+    origin: "*",
+  })
+);
 
 // connect to mongodb
-mongoose.connect('mongodb://localhost:27017');
+mongoose.connect("mongodb://localhost:27017");
 mongoose.Promise = global.Promise;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(express.json());
 // initialize routes
 
-app.use('/api',require('./routes/api'));
-app.use('/api',require('./routes/states_api'));
-app.use('/api',require('./routes/districts_api'));
+app.use("/api", require("./routes/api"));
+app.use("/api", require("./routes/states_api"));
+app.use("/api", require("./routes/districts_api"));
 
 // error handling middleware
-app.use(function(err,req,res,next){
-    //console.log(err);
-    res.status(422).send({error: err.message});
+app.use(function (err, req, res, next) {
+  //console.log(err);
+  res.status(422).send({ error: err.message });
 });
 
 // listen for requests
-app.listen(process.env.port || 4000, function(){
-    console.log('Ready to Go!');
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
