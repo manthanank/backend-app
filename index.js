abd2e283293978c9029616dc811b2903b5ca54af
Manthan Ankolekar <manthan.ank@gmail.com>
2023-02-01 19:26:45 +0530

    updated

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
-// set up our express app
 const app = express();
-
 const cors = require("cors");
+
 app.use(
   cors({
     origin: "*",
   })
 );
 require("dotenv").config();
-// connect to mongodb
-//mongoose.connect("mongodb://localhost:27017");
+
 mongoose
   .connect(
     "mongodb+srv://manthanank:" +
       process.env.MONGO_ATLAS_PW +
       "@cluster0.re3ha3x.mongodb.net/backend"
-    // "mongodb+srv://max:QuBqs0T45GDKPlIG@cluster0-ntrwp.mongodb.net/node-angular?retryWrites=true"
   )
   .then(() => {
     console.log("Connected to database!");
@@ -34,19 +31,17 @@ app.get("/", function (req, res) {
 });
 
 app.use(express.json());
-// initialize routes
 
-app.use("/api", require("./routes/api"));
+app.use("/api", require("./routes/data"));
 app.use("/api", require("./routes/states"));
 app.use("/api", require("./routes/districts"));
+app.use("/api", require("./routes/simpleapi"));
 
-// error handling middleware
+
 app.use(function (err, req, res, next) {
-  //console.log(err);
   res.status(422).send({ error: err.message });
 });
 
-// listen for requests
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
