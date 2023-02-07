6d222a96687312d504ce52ede3af914d7dd46f4c
Manthan Ankolekar <manthan.ank@gmail.com>
2023-02-07 23:24:55 +0530

    updated

--- models/blogs.js
+++ models/blogs.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const Blogs = mongoose.model('blogs', {
     title : {type: String},
-    url: {type: String}
+    url: {type: String},
+    imgurl: {type: String}
 });
 
 module.exports = Blogs;
--- models/projects.js
+++ models/projects.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const Projects = mongoose.model('projects', {
     title : {type: String},
     codeurl: {type: String},
-    previewurl: {type: String}
+    previewurl: {type: String},
+    imgurl: {type: String}
 });
 
 module.exports = Projects;
