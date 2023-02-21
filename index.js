3774afdebdba9f1b934bc9eda8090d4b807486b3
Manthan Ankolekar <manthan.ank@gmail.com>
2023-02-21 10:50:03 +0530

    updated

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require("express");
-// const mongoose = require("mongoose");
-const mongoose = require('./mongodb.js');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-// const mysql = require('mysql2')
 const cloudinary = require('cloudinary').v2;
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+// const mysql = require('mysql2')
 
 // const connection = mysql.createConnection(process.env.DATABASE_URL);
 
@@ -36,28 +34,16 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(express.json());
 
-// mongoose
-//   .connect(
-//     "mongodb+srv://manthanank:" +
-//       process.env.MONGO_ATLAS_PW +
-//       "@cluster0.re3ha3x.mongodb.net/backend"
-//   )
-//   .then(() => {
-//     console.log("Connected to database!");
-//   })
-//   .catch(() => {
-//     console.log("Connection failed!");
-//   });
-
 app.use("/api", require("./routes/data"));
 app.use("/api", require("./routes/users"));
 app.use("/api", require("./routes/states"));
 app.use("/api", require("./routes/districts"));
 app.use("/api", require("./routes/simpleapis"));
 app.use("/api", require("./routes/ossinsight"));
-// app.use(function (err, req, res, next) {
-//   res.status(422).send({ error: err.message });
-// });
+
+app.use(function (err, req, res, next) {
+  res.status(422).send({ error: err.message });
+});
 
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
