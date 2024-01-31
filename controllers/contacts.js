784f366e9bf2c9606c47cccf1df4cfa3e68cc347
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-01-31 22:41:11 +0530

    feat: updated

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,12 +1,31 @@
 const Contact = require('../models/contacts.js');
 
 exports.submitForm = async (req, res, next) => {
+    console.log(req.body);
     try {
+        const { name, email, message } = req.body;
+
+        // Basic validations
+        if (!name || !email || !message) {
+            return res.status(400).json({ message: 'Name, email, and message are required fields' });
+        }
+
+        // // Additional email validation using a regular expression
+        // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        // if (!emailRegex.test(email)) {
+        //     return res.status(400).json({ message: 'Invalid email address' });
+        // }
+
+        // // Validate message length
+        // if (message.length < 10) {
+        //     return res.status(400).json({ message: 'Message should be at least 10 characters long' });
+        // }
+
         // Create a new contact instance
         const newContact = new Contact({
-            name: req.body.name,
-            email: req.body.email,
-            message: req.body.message,
+            name: name,
+            email: email,
+            message: message,
         });
 
         // Save the contact to the database
