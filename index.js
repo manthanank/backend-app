e220c6f13cb2810447dc28929de80f24183ff5c1
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-11-06 22:33:30 +0530

    refactor: reorganize code structure and improve readability

--- index.js
+++ index.js
@@ -1,43 +1,34 @@
 const express = require("express");
-const PORT = process.env.PORT || 3000;
-const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
-const helmet = require("helmet"); // Import helmet
-const logger = require("./logger"); // Import logger
-
+const helmet = require("helmet");
+const logger = require("./logger");
 const otpRoutes = require("./routes/otp");
-
 require("dotenv").config();
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Database connection
 const dbUser = process.env.MONGODB_USER;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DBNAME || "backend";
-
 const mongoURI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.re3ha3x.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-mongoose
-  .connect(mongoURI)
-  .then(() => {
-    logger.info("Connected to MongoDB database!");
-  })
-  .catch((error) => {
-    logger.error("Connection failed:", error.message);
-  });
+mongoose.connect(mongoURI)
+  .then(() => logger.info("Connected to MongoDB database!"))
+  .catch((error) => logger.error("Connection failed:", error.message));
 
-app.use(helmet()); // Use helmet middleware
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// Middleware
+app.use(helmet());
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(express.json());
 
-// routes
+// Routes
 app.use("/api", require("./routes/blogs"));
 app.use("/api", require("./routes/projects"));
 app.use("/api", require("./routes/uses.js"));
@@ -56,15 +47,18 @@ app.use("/api", require("./routes/logs"));
 app.use("/api", require("./routes/notes"));
 app.use("/api", otpRoutes);
 
+// Error handling
 app.use((err, req, res, next) => {
   logger.error(err.stack);
   res.status(500).send("Something went wrong!");
 });
 
+// Serve static files
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
+// Start server
 app.listen(PORT, () => {
   logger.info(`Server started on port ${PORT}`);
 });
