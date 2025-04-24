a087659cfec5c4956deda7e6108e5b81ac7aed5d
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-04-24 10:20:50 +0530

    Update ESLint config to support Jest globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,21 @@ import pluginJs from '@eslint/js';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  { 
+    languageOptions: { 
+      globals: { 
+        ...globals.browser, 
+        ...globals.node 
+      } 
+    } 
+  },
+  {
+    files: ['**/__tests__/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      }
+    }
+  },
   pluginJs.configs.recommended,
 ];
